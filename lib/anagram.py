d8472843d66549eb80c0062d72c9aae5ae0010f7
masinde sylvester <masinde.sylvester@yahoo.com>
2024-12-02 13:56:27 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        # initializing an empty list and checking match words
+        matches = []
+
+        #  iterating throuh the word_list and checking match
+        for word in word_list:
+            if sorted(self.word) == sorted(word):
+                matches.append(word)
+
+        return matches
